f1c71a15f92ffb972bb340aff2f1fd39140e04e5
zhijunyan <zhijunyan@umail.ucsb.edu>
2019-05-09 22:06:12 -0700

    Add files via upload

--- importImage.py
+++ importImage.py
@@ -1,4 +1,279 @@
+
 from PIL import Image
 
-image = Image.open('image.jpg')
-image.show()
+card = raw_input("please type your card:  ")
+print card
+
+
+## heart card
+if (card == "ha"):
+
+	image = Image.open('ha.jpg')
+	image.show()
+
+if (card == "h2"):
+
+	image = Image.open('h2.jpg')
+	image.show()
+
+if (card =="h3"):
+	image = Image.open('h3.jpg')
+	image.show()
+
+if (card =="h4"):
+	image = Image.open('h4.jpg')
+	image.show()
+
+if (card =="h5"):
+	image = Image.open('h5.jpg')
+	image.show()
+
+if (card =="h6"):
+	image = Image.open('h6.jpg')
+	image.show()
+
+if (card =="h7"):
+	image = Image.open('h7.jpg')
+	image.show()
+
+if (card =="h8"):
+	image = Image.open('h8.jpg')
+	image.show()
+
+if (card =="h9"):
+	image = Image.open('h9.jpg')
+	image.show()
+
+if (card =="h10"):
+	image = Image.open('h10.jpg')
+	image.show()
+
+if (card =="hj"):
+	image = Image.open('hj.jpg')
+	image.show()
+
+if (card =="hq"):
+	image = Image.open('hq.jpg')
+	image.show()
+
+if (card =="hk"):
+	image = Image.open('hk.jpg')
+	image.show()
+
+
+
+
+#spade card
+
+if (card == "sa"):
+
+	image = Image.open('sa.jpg')
+	image.show()
+
+if (card == "s2"):
+
+	image = Image.open('s2.jpg')
+	image.show()
+
+if (card =="s3"):
+	image = Image.open('s3.jpg')
+	image.show()
+
+if (card =="s4"):
+	image = Image.open('s4.jpg')
+	image.show()
+
+if (card =="s5"):
+	image = Image.open('s5.jpg')
+	image.show()
+
+if (card =="s6"):
+	image = Image.open('s6.jpg')
+	image.show()
+
+if (card =="s7"):
+	image = Image.open('s7.jpg')
+	image.show()
+
+if (card =="s8"):
+	image = Image.open('s8.jpg')
+	image.show()
+
+if (card =="s9"):
+	image = Image.open('s9.jpg')
+	image.show()
+
+if (card =="s10"):
+	image = Image.open('s10.jpg')
+	image.show()
+
+if (card =="sj"):
+	image = Image.open('sj.jpg')
+	image.show()
+
+if (card =="sq"):
+	image = Image.open('sq.jpg')
+	image.show()
+
+if (card =="sk"):
+	image = Image.open('sk.jpg')
+	image.show()
+
+
+
+##diamond card
+
+if (card == "da"):
+
+	image = Image.open('da.jpg')
+	image.show()
+
+if (card == "d2"):
+
+	image = Image.open('d2.jpg')
+	image.show()
+
+if (card =="d3"):
+	image = Image.open('d3.jpg')
+	image.show()
+
+if (card =="d4"):
+	image = Image.open('d4.jpg')
+	image.show()
+
+if (card =="d5"):
+	image = Image.open('d5.jpg')
+	image.show()
+
+if (card =="d6"):
+	image = Image.open('d6.jpg')
+	image.show()
+
+if (card =="d7"):
+	image = Image.open('d7.jpg')
+	image.show()
+
+if (card =="d8"):
+	image = Image.open('d8.jpg')
+	image.show()
+
+if (card =="d9"):
+	image = Image.open('d9.jpg')
+	image.show()
+
+if (card =="d10"):
+	image = Image.open('d10.jpg')
+	image.show()
+
+if (card =="dj"):
+	image = Image.open('hj.jpg')
+	image.show()
+
+if (card =="dq"):
+	image = Image.open('hq.jpg')
+	image.show()
+
+if (card =="dk"):
+	image = Image.open('hk.jpg')
+	image.show()
+
+
+
+#club card
+
+if (card == "ca"):
+
+	image = Image.open('ca.jpg')
+	image.show()
+
+if (card == "c2"):
+
+	image = Image.open('c2.jpg')
+	image.show()
+
+if (card =="c3"):
+	image = Image.open('c3.jpg')
+	image.show()
+
+if (card =="c4"):
+	image = Image.open('c4.jpg')
+	image.show()
+
+if (card =="c5"):
+	image = Image.open('c5.jpg')
+	image.show()
+
+if (card =="c6"):
+	image = Image.open('c6.jpg')
+	image.show()
+
+if (card =="c7"):
+	image = Image.open('c7.jpg')
+	image.show()
+
+if (card =="c8"):
+	image = Image.open('c8.jpg')
+	image.show()
+
+if (card =="c9"):
+	image = Image.open('c9.jpg')
+	image.show()
+
+if (card =="c10"):
+	image = Image.open('c10.jpg')
+	image.show()
+
+if (card =="cj"):
+	image = Image.open('cj.jpg')
+	image.show()
+
+if (card =="cq"):
+	image = Image.open('cq.jpg')
+	image.show()
+
+if (card =="ck"):
+	image = Image.open('ck.jpg')
+	image.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
